b42928b409680b8eaba2856393bd1d370b8dfede
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2018-12-12 10:43:49 +0100

    added gitignore and script arguments

--- src/kwabot.py
+++ src/kwabot.py
@@ -6,7 +6,8 @@
 
 import datetime
 import requests
-from time import sleep
+from argparse import ArgumentParser
+
 
 class BotHandler:
     """My telegram bot"""
@@ -39,36 +40,42 @@ class BotHandler:
             last_update = get_result[len(get_result)]
         return last_update
 
-GREET_BOT = BotHandler(token)
-GREETINGS = ('hello', 'hi', 'greetings', 'sup')
-NOW = datetime.datetime.now()
 
 def main():
     """Main method"""
+
+    parser = ArgumentParser('Token ID to be passed here')
+    parser.add_argument('--token', type=str, required=True, dest='token')
+    args = parser.parse_args()
+
+    greet_bot = BotHandler(args.token)
+    greetings = ('hello', 'hi', 'greetings', 'sup')
+    now = datetime.datetime.now()
+
     new_offset = None
-    today = NOW.day
-    hour = NOW.hour
+    today = now.day
+    hour = now.hour
 
     while True:
-        GREET_BOT.get_updates(new_offset)
+        greet_bot.get_updates(new_offset)
 
-    last_update = GREET_BOT.get_last_update()
+    last_update = greet_bot.get_last_update()
     last_update_id = last_update['update_id']
 
     last_chat_text = last_update['message']['text']
     last_chat_id = last_update['message']['chat']['id']
     last_chat_name = last_update['message']['chat']['first_name']
 
-    if last_chat_text.lower() in GREETINGS and today == NOW.day and 6 <= hour < 12:
-        GREET_BOT.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
+    if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+        greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
         today += 1
 
-    elif last_chat_text.lower() in GREETINGS and today == NOW.day and 12 <= hour < 17:
-        GREET_BOT.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
+    elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+        greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
         today += 1
 
-    elif last_chat_text.lower() in GREETINGS and today == NOW.day and 17 <= hour < 23:
-        GREET_BOT.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
+    elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+        greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
         today += 1
 
     new_offset = last_update_id + 1
